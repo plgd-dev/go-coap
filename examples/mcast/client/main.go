d78bff508e47d92302505447a58aed3e83362263
Jozef Kralik <jozef.kralik@kistler.com>
2019-01-03 09:52:18 +0100

    publish message where user can set coap options

--- examples/mcast/client/main.go
+++ examples/mcast/client/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 
 	coap "github.com/go-ocf/go-coap"
+	"github.com/ugorji/go/codec"
 )
+
+func decodeMsgToDebug(resp coap.Message, tag string) {
+	var m interface{}
+	err := codec.NewDecoderBytes(resp.Payload(), new(codec.CborHandle)).Decode(&m)
+	out := fmt.Sprintf("RAW:\n%v\n", resp.Payload())
+	if err == nil {
+		bw := new(bytes.Buffer)
+		h := new(codec.JsonHandle)
+		h.BasicHandle.Canonical = true
+		enc := codec.NewEncoder(bw, h)
+		err = enc.Encode(m)
+		if err != nil {
+			log.Printf("Cannot encode %v to JSON: %v", m, err)
+		} else {
+			out = fmt.Sprintf("JSON:\n%v\n", bw.String())
+		}
+	}
+
+	log.Print(
+		"\n-------------------", tag, "------------------\n",
+		"Path: ", resp.PathString(), "\n",
+		"Code: ", resp.Code(), "\n",
+		"Type: ", resp.Type(), "\n",
+		"Query: ", resp.Options(coap.URIQuery), "\n",
+		"ContentFormat: ", resp.Options(coap.ContentFormat), "\n",
+		out,
+	)
+}
 
 func main() {
 	client := &coap.MulticastClient{}
@@ -15,8 +46,13 @@ func main() {
 	}
 
 	sync := make(chan bool)
-	_, err = conn.Publish("/oic/res", func(req *coap.Request) {
-		log.Printf("Got message: %#v from %v", req.Msg, req.Client.RemoteAddr())
+	req, err := conn.NewGetRequest("/oic/res")
+	if err != nil {
+		log.Fatalf("Error sending request: %v", err)
+	}
+	req.SetOption(coap.URIQuery, "rt=oic.wk.d")
+	_, err = conn.PublishMsg(req, func(req *coap.Request) {
+		decodeMsgToDebug(req.Msg, "MCAST")
 		sync <- true
 	})
 	if err != nil {
