615cad3965f5c1b1f099d09ca10945615e083b5b
arun1587 <arunprabhu.1@gmail.com>
2019-03-08 21:26:26 +0100

    add tests for no reponse handling

--- noresponsewriter_test.go
+++ noresponsewriter_test.go
@@ -49,3 +49,54 @@ func TestNoResponseAllCodes(t *testing.T) {
 		t.Fatalf("Expected\n%#v\ngot\n%#v", exp, codes)
 	}
 }
+
+func testNoResponseHandler(t *testing.T, w ResponseWriter, r *Request) {
+	msg := r.Client.NewMessage(MessageParams{
+		Type:      Acknowledgement,
+		Code:      NotFound,
+		MessageID: r.Msg.MessageID(),
+		Token:     r.Msg.Token(),
+	})
+
+	err := w.WriteMsg(msg)
+	if err != nil {
+		if err == ErrMessageNotInterested {
+			t.Fatalf("server unable to write message: %v", err)
+		}
+		return
+	}
+}
+
+func TestNoResponseBehaviour(t *testing.T) {
+	// server creation
+	s, addr, fin, err := RunLocalServerUDPWithHandler("udp", ":", false, BlockWiseSzx16, func(w ResponseWriter, r *Request) { testNoResponseHandler(t, w, r) })
+	defer func() {
+		s.Shutdown()
+		<-fin
+	}()
+	if err != nil {
+		t.Fatalf("Unexpected error '%v'", err)
+	}
+
+	// connect client
+	c := Client{Net: "udp"}
+	con, err := c.Dial(addr)
+	if err != nil {
+		t.Fatalf("Unexpected error '%v'", err)
+	}
+
+	// send client request
+	req := &DgramMessage{
+		MessageBase{
+			typ:       NonConfirmable,
+			code:      GET,
+			messageID: 1234,
+		}}
+
+	// supressing 2XX code: example Content; No error when server sends 4XX response
+	req.SetOption(NoResponse, 2)
+	err = con.WriteMsg(req)
+	if err != nil {
+		t.Fatalf("client unable to write message: %v", err)
+	}
+}
