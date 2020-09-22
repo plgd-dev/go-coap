36b981008e565be4fa9b223c30a7764ded676162
Jozef Kralik <jozef.kralik@kistler.com>
2020-09-22 13:41:46 +0200

    fix cancellation of observation: wait for response

--- tcp/clientobserve.go
+++ tcp/clientobserve.go
@@ -51,7 +51,9 @@ func (o *Observation) Cancel(ctx context.Context) error {
 	defer pool.ReleaseMessage(req)
 	req.SetObserve(1)
 	req.SetToken(o.token)
-	return o.cc.WriteMessage(req)
+	resp, err := o.cc.Do(req)
+	pool.ReleaseMessage(resp)
+	return err
 }
 
 func (o *Observation) wantBeNotified(r *pool.Message) bool {
