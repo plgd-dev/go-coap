6875dba7df538cb6334f06902597e251a2012f04
Jozef Kralik <jozef.kralik@kistler.com>
2020-11-12 07:28:25 +0100

    net.Conn can be used with dtls connection

--- net/conn.go
+++ net/conn.go
@@ -79,7 +79,7 @@ func (c *Conn) WriteWithContext(ctx context.Context, data []byte) error {
 		}
 		err := c.connection.SetWriteDeadline(time.Now().Add(c.heartBeat))
 		if err != nil {
-			return fmt.Errorf("cannot set write deadline for tcp connection: %w", err)
+			return fmt.Errorf("cannot set write deadline for connection: %w", err)
 		}
 		n, err := c.connection.Write(data[written:])
 
@@ -87,7 +87,7 @@ func (c *Conn) WriteWithContext(ctx context.Context, data []byte) error {
 			if isTemporary(err) {
 				continue
 			}
-			return fmt.Errorf("cannot write to tcp connection: %w", err)
+			return fmt.Errorf("cannot write to connection: %w", err)
 		}
 		written += n
 	}
@@ -100,7 +100,7 @@ func (c *Conn) ReadFullWithContext(ctx context.Context, buffer []byte) error {
 	for offset < len(buffer) {
 		n, err := c.ReadWithContext(ctx, buffer[offset:])
 		if err != nil {
-			return fmt.Errorf("cannot read full from tcp connection: %w", err)
+			return fmt.Errorf("cannot read full from connection: %w", err)
 		}
 		offset += n
 	}
@@ -113,22 +113,22 @@ func (c *Conn) ReadWithContext(ctx context.Context, buffer []byte) (int, error) 
 		select {
 		case <-ctx.Done():
 			if ctx.Err() != nil {
-				return -1, fmt.Errorf("cannot read from tcp connection: %v", ctx.Err())
+				return -1, fmt.Errorf("cannot read from connection: %v", ctx.Err())
 			}
-			return -1, fmt.Errorf("cannot read from tcp connection")
+			return -1, fmt.Errorf("cannot read from connection")
 		default:
 		}
 
 		err := c.connection.SetReadDeadline(time.Now().Add(c.heartBeat))
 		if err != nil {
-			return -1, fmt.Errorf("cannot set read deadline for tcp connection: %w", err)
+			return -1, fmt.Errorf("cannot set read deadline for connection: %w", err)
 		}
 		n, err := c.readBuffer.Read(buffer)
 		if err != nil {
 			if isTemporary(err) {
 				continue
 			}
-			return -1, fmt.Errorf("cannot read from tcp connection: %w", err)
+			return -1, fmt.Errorf("cannot read from connection: %w", err)
 		}
 		return n, err
 	}
