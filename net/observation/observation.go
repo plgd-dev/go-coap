25f13fe6c1060704ccb64c126494078c27632151
Jozef Kralik <jozef.kralik@kistler.com>
2020-11-26 13:44:54 +0100

    add tests

--- net/observation/observation.go
+++ net/observation/observation.go
@@ -1,15 +1,21 @@
 package observation
 
-import "time"
+import (
+	"time"
+)
 
 // ObservationSequenceTimeout defines how long is sequence number is valid. https://tools.ietf.org/html/rfc7641#section-3.4
 const ObservationSequenceTimeout = 128 * time.Second
 
 // ValidSequenceNumber implements conditions in https://tools.ietf.org/html/rfc7641#section-3.4
 func ValidSequenceNumber(old, new uint32, lastEventOccurs time.Time, now time.Time) bool {
-	if (old < new && new-old < 2^23) ||
-		(old > new && old-new > 2^23) ||
-		(now.Sub(lastEventOccurs) > ObservationSequenceTimeout) {
+	if old < new && (new-old) < (1<<23) {
+		return true
+	}
+	if old > new && (old-new) > (1<<23) {
+		return true
+	}
+	if now.Sub(lastEventOccurs) > ObservationSequenceTimeout {
 		return true
 	}
 	return false
