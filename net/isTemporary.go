0e85dcd1ee5aa80be95b3d1165b6387eb7b16b38
Jozef Kralik <jozef.kralik@kistler.com>
2020-12-01 08:12:48 +0100

    check time validity only for timeout occurency

--- net/isTemporary.go
+++ net/isTemporary.go
@@ -10,18 +10,22 @@ import (
 const ioTimeout = "i/o timeout"
 
 func isTemporary(err error, deadline time.Time) bool {
-	if deadline.After(time.Now()) {
-		// when connection is closed during TLS handshake, it returns i/o timeout
-		// so we need to validate if timeout real occurs by set deadline otherwise infinite loop occurs.
-		return false
-	}
-
-	if netErr, ok := err.(net.Error); ok && (netErr.Temporary() || netErr.Timeout()) {
-		return true
+	netErr, ok := err.(net.Error)
+	if ok {
+		if netErr.Timeout() {
+			// when connection is closed during TLS handshake, it returns i/o timeout
+			// so we need to validate if timeout real occurs by set deadline otherwise infinite loop occurs.
+			return deadline.Before(time.Now())
+		}
+		if netErr.Temporary() {
+			return true
+		}
 	}
 
 	if strings.Contains(err.Error(), ioTimeout) {
-		return true
+		// when connection is closed during TLS handshake, it returns i/o timeout
+		// so we need to validate if timeout real occurs by set deadline otherwise infinite loop occurs.
+		return deadline.Before(time.Now())
 	}
 	return false
 }
