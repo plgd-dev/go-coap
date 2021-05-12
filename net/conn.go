652aabdb204d4211464c0ab166b9867e8c00e62a
Jozef Kralik <jozef.kralik@kistler.com>
2021-05-12 09:31:00 +0000

    properly set deadline during TLS handshake
    https://github.com/golang/go/issues/31224

--- net/conn.go
+++ net/conn.go
@@ -15,6 +15,8 @@ import (
 type Conn struct {
 	heartBeat  time.Duration
 	connection net.Conn
+
+	handshake  func() error
 	readBuffer *bufio.Reader
 	lock       sync.Mutex
 }
@@ -43,6 +45,10 @@ func NewConn(c net.Conn, opts ...ConnOption) *Conn {
 		heartBeat:  cfg.heartBeat,
 		readBuffer: bufio.NewReaderSize(c, 2048),
 	}
+	if v, ok := c.(interface{ Handshake() error }); ok {
+		connection.handshake = v.Handshake
+	}
+
 	return &connection
 }
 
@@ -77,8 +83,12 @@ func (c *Conn) WriteWithContext(ctx context.Context, data []byte) error {
 			return ctx.Err()
 		default:
 		}
+		err := c.doHandshakeLocked(ctx, nil)
+		if err != nil {
+			return fmt.Errorf("cannot TLS handshake: %w", err)
+		}
 		deadline := time.Now().Add(c.heartBeat)
-		err := c.connection.SetWriteDeadline(deadline)
+		err = c.connection.SetWriteDeadline(deadline)
 		if err != nil {
 			return fmt.Errorf("cannot set write deadline for connection: %w", err)
 		}
@@ -91,7 +101,7 @@ func (c *Conn) WriteWithContext(ctx context.Context, data []byte) error {
 				}
 				continue
 			}
-			return fmt.Errorf("cannot write to connection: %w", err)
+			return err
 		}
 		written += n
 	}
@@ -111,20 +121,68 @@ func (c *Conn) ReadFullWithContext(ctx context.Context, buffer []byte) error {
 	return nil
 }
 
+// During handshake wee need to use setDeadline because https://github.com/golang/go/issues/31224
+// added comment in https://github.com/golang/go/commit/c9b9cd73bb7a7828d34f4a7844f16c3fbc0674dd
+func (c *Conn) doHandshakeLocked(ctx context.Context, onTimeout func() error) error {
+	if c.handshake == nil {
+		return nil
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+			return fmt.Errorf("handshake failed")
+		default:
+		}
+		deadline := time.Now().Add(c.heartBeat)
+		err := c.connection.SetDeadline(deadline)
+		if err != nil {
+			return fmt.Errorf("cannot set deadline for handshake: %w", err)
+		}
+		err = c.handshake()
+		if err != nil {
+			if isTemporary(err, deadline) {
+				if onTimeout != nil {
+					err := onTimeout()
+					if err != nil {
+						return fmt.Errorf("on timeout returns error: %w", err)
+					}
+				}
+				continue
+			}
+		}
+		return err
+	}
+}
+
+func (c *Conn) doHandshake(ctx context.Context, onTimeout func() error) error {
+	if c.handshake == nil {
+		return nil
+	}
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.doHandshakeLocked(ctx, onTimeout)
+}
+
 // ReadWithContext reads stream with context.
 func (c *Conn) ReadWithContext(ctx context.Context, buffer []byte) (int, error) {
 	for {
 		select {
 		case <-ctx.Done():
 			if ctx.Err() != nil {
-				return -1, fmt.Errorf("cannot read from connection: %v", ctx.Err())
+				return -1, ctx.Err()
 			}
 			return -1, fmt.Errorf("cannot read from connection")
 		default:
 		}
-
+		err := c.doHandshake(ctx, nil)
+		if err != nil {
+			return -1, fmt.Errorf("cannot TLS handshake: %w", err)
+		}
 		deadline := time.Now().Add(c.heartBeat)
-		err := c.connection.SetReadDeadline(deadline)
+		err = c.connection.SetReadDeadline(deadline)
 		if err != nil {
 			return -1, fmt.Errorf("cannot set read deadline for connection: %w", err)
 		}
@@ -133,7 +191,7 @@ func (c *Conn) ReadWithContext(ctx context.Context, buffer []byte) (int, error) 
 			if isTemporary(err, deadline) {
 				continue
 			}
-			return -1, fmt.Errorf("cannot read from connection: %w", err)
+			return -1, err
 		}
 		return n, err
 	}
