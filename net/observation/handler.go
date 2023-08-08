906eb114703ab2c7364e4b6779f5681bf3bcd8a3
Daniel Adam <daniel.adam1922@protonmail.com>
2023-08-08 10:22:34 +0200

    net: Enhance observation management with ETag tracking
    This commit introduces an enhancement to the observation mechanism
    in the network module by implementing ETag tracking. The latest ETag
    value associated with each observation is now stored in the internal
    representation.
    
    With this update, each incoming message containing the ETag CoAP option
    automatically updates the ETag value for the relevant observation.
    When an observation is canceled, the stored ETag value is utilized.
    When the server detects a valid ETag match, it now responds with a
    VALID code and an empty payload instead of resending the content
    of the resource. This optimization minimizes unnecessary data transfer
    and reduces network load.

--- net/observation/handler.go
+++ net/observation/handler.go
@@ -146,10 +146,11 @@ type Observation[C Client] struct {
 	waitForResponse     atomic.Bool
 	observationHandler  *Handler[C]
 
-	private struct {
+	private struct { // members guarded by mutex
 		mutex       sync.Mutex
-		obsSequence uint32    // guarded by mutex
-		lastEvent   time.Time // guarded by mutex
+		obsSequence uint32
+		lastEvent   time.Time
+		etag        []byte
 	}
 }
 
@@ -199,6 +200,12 @@ func (o *Observation[C]) Request() message.Message {
 	return o.req
 }
 
+func (o *Observation[C]) etag() []byte {
+	o.private.mutex.Lock()
+	defer o.private.mutex.Unlock()
+	return o.private.etag
+}
+
 // Cancel remove observation from server. For recreate observation use Observe.
 func (o *Observation[C]) Cancel(ctx context.Context, opts ...message.Option) error {
 	if !o.cleanUp() {
@@ -217,6 +224,10 @@ func (o *Observation[C]) Cancel(ctx context.Context, opts ...message.Option) err
 		}
 	}
 	req.SetToken(o.req.Token)
+	etag := o.etag()
+	if etag != nil {
+		_ = req.SetETag(etag) // ignore invalid etag
+	}
 	resp, err := o.observationHandler.do(req)
 	if err != nil {
 		return err
@@ -240,6 +251,9 @@ func (o *Observation[C]) wantBeNotified(r *pool.Message) bool {
 	if ValidSequenceNumber(o.private.obsSequence, obsSequence, o.private.lastEvent, now) {
 		o.private.obsSequence = obsSequence
 		o.private.lastEvent = now
+		if etag, err := r.ETag(); err == nil {
+			o.private.etag = etag
+		}
 		return true
 	}
 
