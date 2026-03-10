7eb30bacaa851399be13b1ef6826f1f031b57972
Theodor Rauch <theodor.rauch@ml-pa.com>
2026-03-10 18:51:40 +0100

    fix: comment

--- udp/server/server.go
+++ udp/server/server.go
@@ -162,7 +162,7 @@ func (s *Server) Serve(l *coapNet.UDPConn) error {
 
 		// UDPConn.LocalAddr() only takes into account the address it is bound to.
 		// In the case of a wildcard address, the actual destination address is in the control message.
-		// Copy the UDPAddr before mutating to avoid a data race with concurrent LocalAddr() readers.
+		// On server-initiated exchanges, listeners Local Addr can be used as the client has no assumptions of the source.
 		laddrVal := *l.LocalAddr().(*net.UDPAddr)
 		laddr := &laddrVal
 		if cm != nil && cm.Dst != nil {
