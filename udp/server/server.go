90efe21401c0d64f00b8c2b1e7549e789f939478
Theodor Rauch <theodor.rauch@ml-pa.com>
2026-03-10 09:36:33 +0100

    fix: data race condition

--- udp/server/server.go
+++ udp/server/server.go
@@ -162,7 +162,9 @@ func (s *Server) Serve(l *coapNet.UDPConn) error {
 
 		// UDPConn.LocalAddr() only takes into account the address it is bound to.
 		// In the case of a wildcard address, the actual destination address is in the control message.
-		laddr := l.LocalAddr().(*net.UDPAddr)
+		// Copy the UDPAddr before mutating to avoid a data race with concurrent LocalAddr() readers.
+		laddrVal := *l.LocalAddr().(*net.UDPAddr)
+		laddr := &laddrVal
 		if cm != nil && cm.Dst != nil {
 			laddr.IP = cm.Dst
 		}
